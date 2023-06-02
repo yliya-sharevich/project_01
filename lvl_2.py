8cb9e9af5a5e1d118f85f6d27b06c5a2ebaaa0d0
ЮЛИЯ <129542233+yliya-sharevich@users.noreply.github.com>
2023-06-02 12:59:05 +1000

    Add files via upload

--- lvl_2.py
+++ lvl_2.py
@@ -1,5 +1,24 @@
 # Задача 2.1
 
+arr = [4, 6, 2, 1, 9, 63, -134, 566]
+       
+
+
+def minimum(arr):
+ result = float()
+ for num in arr:
+   if num < result:
+    result = num
+    return result
+def maximum(arr):
+ result = float()
+ for num in arr:
+   if num > result:
+    result = num
+ return result
+print('min =', minimum(arr), 'max =', maximum(arr)) 
+
+
 # Задача 2.2
 def квартал(month):
     if month in [1, 2, 3]:
@@ -30,16 +49,29 @@ def switch_it_up(number):
         0: "ноль"
     }
     return nums[number]
+print(switch_it_up(2))
 
-print(switch_it_up(number))
 
 
-# Задача 2.4
+# Задача 2.4.1
 
-s = "Hi! Hello!"
+import re   
+inp_str = "Hi! Hello!"
 
-def remove_exclamation_marks(s):
- return s.replace("!", "")
+marks = "!"
+for x in inp_str:  
+    if x in marks:  
+        opt_str = inp_str.replace(x, "")
+print(opt_str)
+
+# Задача 2.4.2
+
+Str = "Hi!!!"
+l = len(Str)
+
+Remove_last = Str[:l-1]
+
+print(Remove_last)
 
 
 
